e37ce4cf87c9fdee1cd188dc6f5f376b33ac87ef
Gurcan Hamali <gurcanhamali@gmail.com>
2020-12-26 18:17:44 +1100

    429. More on useState() & State Updating

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -21,7 +21,13 @@ const IngredientForm = React.memo(props => {
               type='text'
               id='title'
               value={inputState[0].state}
-              onChange={event => inputState[1]({ title: event.target.value })}
+              onChange={event => {
+                const newTitle = event.target.value;
+                inputState[1](prevState => ({
+                  title: newTitle,
+                  amount: prevState.amount
+                }));
+              }}
             />
           </div>
           <div className='form-control'>
@@ -30,7 +36,13 @@ const IngredientForm = React.memo(props => {
               type='number'
               id='amount'
               value={inputState[0].amount}
-              onChange={event => inputState[1]({ amount: event.target.value })}
+              onChange={event => {
+                const newAmount = event.target.value;
+                inputState[1](prevState => ({
+                  amount: newAmount,
+                  title: prevState.title
+                }));
+              }}
             />
           </div>
           <div className='ingredient-form__actions'>
